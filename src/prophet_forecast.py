1fabed931043990781eaa612f2a636eb2b3f169e
Tony Fraser <tony.fraser@gmail.com>
2023-03-09 08:56:53 -0500

    Update prophet_forecast.py

--- src/prophet_forecast.py
+++ src/prophet_forecast.py
@@ -1,6 +1,9 @@
 """
 # A Simple Prophet Forecast
 
+Both inbound and outbound data is stored
+in the data directory.
+
 """
 import pandas as pd
 from prophet import Prophet
@@ -15,12 +18,11 @@ df['pandas_date']=pd.DatetimeIndex(df['year'] + '-' + df['month']+ '-' + df['day
 df.drop(['Product', 'Store', 'StringDate','year','month','day'], axis=1, inplace=True)
 
 # prophet uses only two columns to forecast, 
-#     what you are measuring (value), and
-#     the pandas data time column 
+#     what you are measuring, the value, "Y", and
+#     the pandas date stamp column, "DS" 
 # so, let's rename them in place. 
 
 df.columns = ['y', 'ds']
-
 # In [9]: df.dtypes
 # Out[9]:
 # y            float64
@@ -28,18 +30,22 @@ df.columns = ['y', 'ds']
 # dtype:       object
 
 
-# good luck with this. In the read me I tried to cover how
-# to get supporting libraries installed. 
+# good luck with this. In the readme I tried to cover how
+# to get supporting libraries installed.
 
 prophet = Prophet(interval_width=.95, daily_seasonality=True)
+
+#NOTE!! Propet is not an immutable object, we are about to change it.
 model = prophet.fit(df)
+
 future = prophet.make_future_dataframe(periods=100, freq='D')
 forecast = prophet.predict(future)
 
-## If you have plotly installed and working ... 
-# Shows lots of cool stuff to explore
+## If you have plotly installed and working.. 
+# Shows you a little more about your data, like weeks or performance
+# by day. (basically, prophet knows a lot about days)
 prophet.plot_components(forecast).show()
-# Just plot the data.
+# The graph you'd expect, an estimate of sales based on date. 
 prophet.plot(forecast).show()
 # Save to FS so you can see the forecat output.
 
@@ -49,9 +55,10 @@ b = Bootstrap()
 spark = b.get_spark()
 forecastSpark = spark.createDataFrame(forecast)
 dfSpark = spark.createDataFrame(df)
-#looks like the same data, but just 100 days longer.
+#looks like the same data, but just 100 days more.
 dfSpark.select(min("ds").alias("min_df"), max("ds").alias("max_df"), count("ds").alias("count")).show()
 forecastSpark.select(min("ds").alias("min_forecast"), max("ds").alias("max_forecast"), count("ds").alias("count")).show()
+
 # +-------------------+-------------------+-----+
 # |             min_df|             max_df|count|
 # +-------------------+-------------------+-----+
@@ -65,7 +72,7 @@ forecastSpark.select(min("ds").alias("min_forecast"), max("ds").alias("max_forec
 # +-------------------+-------------------+-----+
 
 
-# save as pandas and/or spark
+# save if you want.
 forecast.head(500).to_csv('./data/pandas_prophet_forecast_output.csv') 
 forecastSpark.limit(500) \
     .repartition(1) \
